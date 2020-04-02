ca519f97150b32b4a9d98f739b21acae77611551
Gaurav Maheshwari <dc.gaurav@gmail.com>
2020-04-02 14:55:53 -0700

    added tfstate on blob

--- vm.tf
+++ vm.tf
@@ -1,7 +1,15 @@
 provider "azurerm" {
     version = 1.38
     }
+terraform{
+    backend "azurerm"{
+        resource_group_name="Terraform"
+        storage_account_name="tfstatefileterraform"
+        container_name ="tfstatefile"
+        key ="terraformvmcreation.tfstate"
 
+    }
+}
 
 #Deploy Public IP
 resource "azurerm_public_ip" "example" {
