b50c777c7621fa036174f35907c8943dad2f44b9
Gaurav Maheshwari <dc.gaurav@gmail.com>
2020-04-03 08:38:20 -0700

    commit

--- vm.tf
+++ vm.tf
@@ -144,7 +144,7 @@ resource "azurerm_virtual_machine" "drsnap1" {
   }
 
   storage_os_disk {
-    name              = "osdisk1"
+    name              = "osdisk2"
     disk_size_gb      = "40"
     caching           = "ReadWrite"
     create_option     = "FromImage"
